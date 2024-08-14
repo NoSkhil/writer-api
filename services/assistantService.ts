1f9db0e288d5a77c6808d09472d664476b5ecf26
NoSkhil <nshyamakhil@gmail.com>
2024-08-14 04:14:02 -0400

    createAsisstantMessage service

--- services/assistantService.ts
+++ services/assistantService.ts
@@ -4,6 +4,8 @@ const openai = new OpenAI();
 
 type IAssistantThread = OpenAI.Beta.Threads.Thread;
 type ICreateAssistantThread = OpenAI.Beta.Threads.ThreadCreateParams;
+type ICreateAssistantMessage = OpenAI.Beta.Threads.MessageCreateParams;
+type IAssistantMessage = OpenAI.Beta.Threads.Message;
 
 const getAssistantThread = async(id: string): Promise<Record<"data",IAssistantThread> | Record<"err",string>> => {
     try {
@@ -18,10 +20,10 @@ const getAssistantThread = async(id: string): Promise<Record<"data",IAssistantTh
     }
 };
 
-const createAssistantThread = async(messageData: ICreateAssistantThread): Promise<IAssistantThread> => {
+const createAssistantThread = async(messageData: ICreateAssistantThread): Promise<Record<"data",IAssistantThread>> => {
     try {
-        const createdMessage = await openai.beta.threads.create(messageData);
-        return createdMessage;
+        const createdThread = await openai.beta.threads.create(messageData);
+        return {data:createdThread};
     }
     catch(err) {
         console.log(err);
@@ -29,7 +31,23 @@ const createAssistantThread = async(messageData: ICreateAssistantThread): Promis
     }
 };
 
+const createAssistantMessage = async(threadId:string, messageData: ICreateAssistantMessage): Promise<Record<"data",IAssistantMessage> | Record<"err",string>> => {
+    try {
+        const threadData = await getAssistantThread(threadId);
+        if ("err" in threadData) return {err:"Invalid Assistant Thread ID."};  
+        const {data:thread} = threadData;
+
+        const createMessage = await openai.beta.threads.messages.create(thread.id,messageData);
+        return {data:createMessage};
+    }
+    catch(err) {
+        console.log(err);
+        throw err;
+    }
+}
+
 export default {
     getAssistantThread,
-    createAssistantThread
+    createAssistantThread,
+    createAssistantMessage
 };
